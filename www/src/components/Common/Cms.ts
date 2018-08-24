8d4701b90a4861dd71d7429a559dbf1e1811e930
Yuanlong Li <mastermist@gmail.com>
2018-08-24 07:40:53 +0200

    * es5

--- www/src/components/Common/Cms.ts
+++ www/src/components/Common/Cms.ts
@@ -1,4 +1,4 @@
 import Strapi from 'strapi-sdk-javascript';
-//declare var __API__: string;
+declare var __API__: string;
 
-export default new Strapi('https://cms.cocatiel.co');//__API__);
+export default new Strapi(__API__);
